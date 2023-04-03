111f5b696ccbdad7b264857d1252e532d566de6d
Rahul kapoor <kapoorrahul304@gmail.com>
2023-04-03 12:52:10 +0530

    assignment complete

---
+++ block-BJaajc/code/index.js
@@ -0,0 +1,13 @@
+function getFullName(firstName, lastName){
+return `${firstName} ${lastName}`;
+}
+
+function isPallindrome(str) {
+    if (str.split("").reverse().join("")) {
+      return true;
+    } else false;
+  }
+  
+  module.exports = getFullName;
+  // module.exports = isPallindrome;
+  
